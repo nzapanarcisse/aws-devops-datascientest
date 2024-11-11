2d239fa046f7729ff0a009c92b681c6fda1d01fd
nzapanarcisse <nzapanarcisse@gmail.com>
2024-11-12 00:27:17 +0400

    Update deploy_release_info.sh

--- scripts/deploy_release_info.sh
+++ scripts/deploy_release_info.sh
@@ -5,7 +5,7 @@ PATH=$PATH:/usr/local/bin/
 yum install python3-pip -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
-pip3 install git-remote-codecommit
+sudo yum install git -y 
 rm -Rf alpinehelloworld || echo "folder already deleted"
 rm -Rf /root/deployment || echo "folder already deleted"
 git clone https://github.com/nzapanarcisse/aws-devops-datascientest.git ./alpinehelloworld
