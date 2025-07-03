1b10a71848d725430d3affe743e7f580e53a26ff
nzapanarcisse <nzapanarcisse@gmail.com>
2025-07-03 17:36:28 +0400

    Update validate_application.sh

--- scripts/validate_application.sh
+++ scripts/validate_application.sh
@@ -5,4 +5,5 @@ PATH=$PATH:/usr/local/bin
 namespace=$(grep -r "namespace" /root/deployment/kustomization.yaml | cut -f2 -d' ')
 sleep 60
 url=$(kubectl get svc alpinehelloworld-service  --kubeconfig /root/.kube/config -n $namespace -o jsonpath="{.status.loadBalancer.ingress[*].hostname}")
+sleep 60
 curl http://$url | grep -q "Proven-FR"
